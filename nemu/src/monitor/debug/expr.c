03a2ca4587edf21b6b3990c5852b39d848d1349e
tracer-ics2020 <tracer@njuics.org>
2020-09-25 00:00:03 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 43cd2054b7b542f0939176aef793756e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  00:00:03 up  4:17,  6 users,  load average: 0.16, 0.11, 0.12

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -105,7 +105,6 @@ static bool make_token(char *e) {
 }
 
 static bool judge(int p,int q,bool *success){
-  if(tokens[p].type!='('||tokens[q].type!=')')return false;
   int head=0;
   for(int i=p;i<q;i++){
     if(tokens[i].type=='('){
@@ -138,7 +137,7 @@ static word_t eval(int p,int q,bool *success){
     }
     return atoi(tokens[p].str);
   }
-  else if(judge(p+1,q-1,success)){
+  else if(tokens[p].type=='('&&tokens[q].type==')'&&judge(p+1,q-1,success)){
     return eval(p+1,q-1,success);
   }
   else{
