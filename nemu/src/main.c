33148b565f2802dd96ae1cd61f02c35a29e0a760
tracer-ics2020 <tracer@njuics.org>
2020-09-25 20:03:54 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: cbf908fd8c2c44fbbc3fa9e13d943170     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  20:03:54 up  1:35,  1 user,  load average: 0.65, 0.59, 0.63

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,3 +1,5 @@
+#include "monitor/debug/expr.h"
+#include<stdlib.h>
 void init_monitor(int, char *[]);
 void engine_start();
 int is_exit_status_bad();
@@ -6,6 +8,13 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
   
+   for(int i=1;i<argc;i++){
+    bool *success=(bool*)malloc(sizeof(bool));
+    *success=true;
+    bool *flag=(bool*)malloc(sizeof(bool));
+    *flag=true;
+    expr(argv[i],success,flag);
+  }
   /* Start engine. */
   engine_start();
 
