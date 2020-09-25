4e3456c04d915d72940baa80f2797d1e3fac8d0e
tracer-ics2020 <tracer@njuics.org>
2020-09-25 23:53:31 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: a2671cba101a4fd1bacff09e87867d36     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:53:31 up 24 min,  1 user,  load average: 0.62, 0.41, 0.30

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -7,7 +7,25 @@ int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
-  
+  //begin test
+  FILE *fp;
+  fp=fopen("/tools/gen-expr/input","r");
+  char buf[65536];
+  fscanf(fp,"%s",buf);
+  while(fgets(buf,65536,fp)!=NULL){
+    char *arg1=strtok(buf," ");
+    bool *success1=(bool*)malloc(sizeof(bool));
+    *success1=true;
+    bool *flag1=(bool*)malloc(sizeof(bool));
+    *flag1=true;
+    unsigned int ans1=expr(buf,success1,flag1);
+    if(atoi(arg1)==ans1){
+      printf("correct! %u\n",ans1);
+    }
+    else{
+      printf("wrong! the answer is %u, gets %u",atoi(arg1),ans1);
+    }
+  }
   /* Start engine. */
   engine_start();
 
