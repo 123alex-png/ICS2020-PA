fb698f042f4af1469d624a55c8bc7d621f497e9b
tracer-ics2020 <tracer@njuics.org>
2020-10-04 12:11:39 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 234a8b811b614183a69c8cf805c8f5f8     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:11:39 up  1:13,  1 user,  load average: 0.18, 0.16, 0.18

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -8,23 +8,24 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
   /**begin test*/
-  // FILE *fp;
-  // fp=fopen("tools/gen-expr/input","r");
-  // char buf[65536];
-  // int cnt=0;
-  // while(fgets(buf,65536,fp)!=NULL){
-  //   char *arg1=strtok(buf," ");
-  //   char *arg2=arg1+strlen(arg1)+1;
-  //   bool *success1=(bool*)malloc(sizeof(bool));
-  //   *success1=true;
-  //   unsigned int ans1=expr(arg2,success1);
-  //   if(atoi(arg1)==ans1){
-  //     printf("%d:correct! %u\n",cnt++,ans1);
-  //   }
-  //   else{
-  //     printf("%d:wrong! the answer is %u, gets %u\n",cnt++,atoi(arg1),ans1);
-  //   }
-  // }
+  FILE *fp;
+  fp=fopen("tools/gen-expr/input","r");
+  char buf[65536];
+  int cnt=0;
+  while(fgets(buf,65536,fp)!=NULL){
+    memset(buf,0,sizeof(buf));
+    char *arg1=strtok(buf," ");
+    char *arg2=arg1+strlen(arg1)+1;
+    bool *success1=(bool*)malloc(sizeof(bool));
+    *success1=true;
+    unsigned int ans1=expr(arg2,success1);
+    if(atoi(arg1)==ans1){
+      printf("%d:correct! %u\n",cnt++,ans1);
+    }
+    else{
+      printf("%d:wrong! the answer is %u, gets %u\n",cnt++,atoi(arg1),ans1);
+    }
+  }
   /***************end test***************/
   /* Start engine. */
   engine_start();
