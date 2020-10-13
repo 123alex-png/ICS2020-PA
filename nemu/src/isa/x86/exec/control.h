e28b03bc1ddc1152051bf2deab7f70988b18f251
tracer-ics2020 <tracer@njuics.org>
2020-10-13 18:26:40 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 4ab1284bf7ac4a0280b8df56062bf9b6     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:26:40 up 11:07,  1 user,  load average: 0.35, 0.20, 0.21

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -25,7 +25,7 @@ static inline def_EHelper(jmp_rm) {
 static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
   rtl_push(s,&(cpu.pc));
-  rtl_j(s,*(s->dest.preg));
+  rtl_j(s,cpu.pc+*(s->dest.preg));
   print_asm("call %x", s->jmp_pc);
 }
 
