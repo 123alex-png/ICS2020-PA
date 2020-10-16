936e92901c6e84b652362ea372befb1ad6f351de
tracer-ics2020 <tracer@njuics.org>
2020-10-16 22:19:36 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: d7c3d9194d8d4098b4dd8eb128d17413     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:19:36 up  8:43,  1 user,  load average: 0.73, 0.29, 0.23

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -69,7 +69,7 @@ static inline def_rtl(is_add_carry, rtlreg_t* dest,
     cpu.f=*src;\
   } \
   static inline def_rtl(concat(get_, f), rtlreg_t* dest) { \
-    TODO();\
+    *dest=cpu.f;\
   }
 
 def_rtl_setget_eflags(CF)
