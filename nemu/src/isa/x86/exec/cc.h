9222b8264ef69e4f930beb97235ddb70e91603c1
tracer-ics2020 <tracer@njuics.org>
2020-10-19 18:07:22 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: d7c3d9194d8d4098b4dd8eb128d17413     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:07:22 up 13:29,  1 user,  load average: 0.57, 0.41, 0.48

--- nemu/src/isa/x86/exec/cc.h
+++ nemu/src/isa/x86/exec/cc.h
@@ -29,16 +29,22 @@ static inline void rtl_setcc(DecodeExecState *s, rtlreg_t* dest, uint32_t subcod
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
+    case CC_O:rtl_li(s,dest,cpu.OF);break;
+    case CC_B:rtl_li(s,dest,cpu.CF);break;
     case CC_E:rtl_li(s,dest,cpu.ZF);break;
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-       TODO();
+    case CC_BE:rtl_li(s,dest,c_or(cpu.CF,cpu.ZF));
+    case CC_S:rtl_li(s,dest,cpu.SF);break;
+    case CC_L:rtl_li(s,dest,interpret_relop(RELOP_NE,cpu.SF,cpu.OF));break;
+    case CC_LE:rtl_li(s,dest,c_or(cpu.ZF,interpret_relop(RELOP_NE,cpu.SF,cpu.OF)));break;
+    case CC_NO:rtl_li(s,dest,!cpu.OF);break;
+    case CC_NB:rtl_li(s,dest,!cpu.CF);break;
+    case CC_NE:rtl_li(s,dest,!cpu.ZF);break;
+    case CC_NBE:rtl_li(s,dest,!c_and(cpu.CF,cpu.ZF));break;
+    case CC_NS:rtl_li(s,dest,!cpu.SF);break;
+    case CC_NL:rtl_li(s,dest,interpret_relop(RELOP_EQ,cpu.SF,cpu.OF));break;
+    case CC_NLE:rtl_li(s,dest,c_and(!cpu.ZF,interpret_relop(RELOP_EQ,cpu.SF,cpu.OF)));break;
     default: panic("should not reach here");
-    case CC_P: panic("PF is not supported");
+    case CC_P:case CC_NP: panic("PF is not supported");
   }
 
   if (invert) {
