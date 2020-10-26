adef5a1ba9a88c8cc13189b19db577c299ea6c5f
tracer-ics2020 <tracer@njuics.org>
2020-10-26 23:27:06 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 90e6e32c87544185b84cdedaabcd7254     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:27:06 up  7:06,  1 user,  load average: 0.04, 0.08, 0.10

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -28,7 +28,7 @@ int sprintf(char *out, const char *fmt, ...) {
           int tmp=va_arg(arg,int);
           if(tmp<0){
             out[cnt++]='-';
-            tmp=(unsigned)(tmp);
+            tmp=(unsigned)(-tmp);
           }
           else{
             tmp=(unsigned)tmp;
