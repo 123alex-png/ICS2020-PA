4df68e642c0f4ffafe3420f20fc1142bca44765e
tracer-ics2020 <tracer@njuics.org>
2020-10-26 22:35:48 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 90e6e32c87544185b84cdedaabcd7254     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:35:48 up  6:15,  1 user,  load average: 0.15, 0.25, 0.17

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,9 +14,9 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list arg;
+  // va_list arg;
   int cnt=0;
-  va_start(arg,fmt);
+  // va_start(arg,fmt);
   for(size_t i=0;fmt[i]!='\0';i++){
     if(fmt[i]!='%'){
       out[cnt++]=fmt[i];
@@ -25,7 +25,7 @@ int sprintf(char *out, const char *fmt, ...) {
       i++;
       switch(fmt[i]){
         case 'd':{
-          int tmp=va_arg(arg,int);
+          int tmp=/*va_arg(arg,int)*/10;
           if(tmp<0){
             out[cnt++]='-';
             tmp=(unsigned)(-tmp);
@@ -63,7 +63,7 @@ int sprintf(char *out, const char *fmt, ...) {
     }
   }
   out[cnt++]='\0';
-  va_end(arg);
+  // va_end(arg);
   return cnt;
 }
 
