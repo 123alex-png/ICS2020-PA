62b2b66fa182f333945accd0b4e122b603d88c30
tracer-ics2020 <tracer@njuics.org>
2020-10-26 12:54:08 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 90e6e32c87544185b84cdedaabcd7254     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:54:08 up 4 min,  1 user,  load average: 0.13, 0.19, 0.09

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -4,8 +4,15 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  // union isa_gdb_regs r;
-
+  if(ref_r->eax!=cpu.eax)return false;
+  if(ref_r->edx!=cpu.edx)return false;
+  if(ref_r->ecx!=cpu.ecx)return false;
+  if(ref_r->ebx!=cpu.ebx)return false;
+  if(ref_r->esp!=cpu.esp)return false;
+  if(ref_r->ebp!=cpu.ebp)return false;
+  if(ref_r->esi!=cpu.esi)return false;
+  if(ref_r->edi!=cpu.edi)return false;
+  if(ref_r->pc!=cpu.pc)return false;
   return true;
 }
 
