1ddba7ddb28c7b151a97f32f7a470ba9aa4d72ca
tracer-ics2020 <tracer@njuics.org>
2020-10-26 12:59:23 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 90e6e32c87544185b84cdedaabcd7254     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:59:23 up 9 min,  1 user,  load average: 0.01, 0.14, 0.10

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -12,7 +12,7 @@ void init_difftest(char *ref_so_file, long img_size, int port);
 
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
-static char *img_file = "/home/quli/ics2020/am-kernels/tests/cpu-tests/build/dummy-x86-nemu.bin";
+static char *img_file = "/home/quli/ics2020/am-kernels/tests/cpu-tests/build/recursion-x86-nemu.bin";
 static int batch_mode = false;
 static int difftest_port = 1234;
 
