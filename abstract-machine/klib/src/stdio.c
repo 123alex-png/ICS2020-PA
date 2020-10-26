0eb74b66a5afca73f649916980f4a7f8d23d95cc
tracer-ics2020 <tracer@njuics.org>
2020-10-26 22:51:13 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 90e6e32c87544185b84cdedaabcd7254     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:51:13 up  6:30,  1 user,  load average: 0.32, 0.20, 0.17

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -17,56 +17,46 @@ int sprintf(char *out, const char *fmt, ...) {
   va_list arg;
   int cnt=0;
   va_start(arg,fmt);
-  // for(size_t i=0;fmt[i]!='\0';i++){
-    // if(fmt[i]!='%'){
-    //   out[cnt++]=fmt[i];
-    // }
-    // else{
-      // int i=1;
-      // switch(fmt[i]){
-      //   case 'd':{
+  for(size_t i=0;fmt[i]!='\0';i++){
+    if(fmt[i]!='%'){
+      out[cnt++]=fmt[i];
+    }
+    else{
+      int i=1;
+      switch(fmt[i]){
+        case 'd':{
           int tmp=va_arg(arg,int);
-          assert(tmp==10);
-          out[0]='1';out[1]='0';
-          return tmp;
-          // if(tmp<0){
-          //   out[cnt++]='-';
-          //   tmp=(unsigned)(-tmp);
-          // }
-          // else{
-          //   tmp=(unsigned)tmp;
-          // }
-          // char digit[15];//from low to high
-          // int pos=0;
-          // do{
-          //   digit[pos++]=tmp%10+'0';
-          //   tmp/=10;
-
-            // digit[0]=tmp%10+'0';
-            // tmp/=10;
-            // digit[1]=tmp%10+'0';
-            // tmp/=10;
-            
-          // }while(tmp!=0);
-          // out[cnt++]=digit[1];
-          // out[cnt++]=digit[0];
-          // break;
-          // while(pos>0){
-          //   --pos;
-          //   out[cnt++]=digit[pos];
-          // }//break;
-        // }
-        // case 's':{
-        //   char *tmp=va_arg(arg,char *);
-        //   for(size_t i=0;*(tmp+i)!='\0';i++){
-        //     out[cnt++]=*(tmp+i);
-        //   }
-        //   break;
-        // }
-      // }
-  //   }
-  // }
-  // out[cnt++]='\0';
+          if(tmp<0){
+            out[cnt++]='-';
+            tmp=(unsigned)(-tmp);
+          }
+          else{
+            tmp=(unsigned)tmp;
+          }
+          char digit[15];//from low to high
+          int pos=0;
+          do{
+            digit[pos++]=tmp%10+'0';
+            tmp/=10; 
+          }while(tmp!=0);
+          break;
+          while(pos>0){
+            --pos;
+            out[cnt++]=digit[pos];
+          }
+          break;
+        }
+        case 's':{
+          char *tmp=va_arg(arg,char *);
+          for(size_t i=0;*(tmp+i)!='\0';i++){
+            out[cnt++]=*(tmp+i);
+          }
+          break;
+        }
+      }
+    }
+  }
+  out[cnt++]='\0';
    va_end(arg);
   return cnt;
 }
