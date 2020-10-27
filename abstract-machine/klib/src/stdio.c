bc5301fa9cdabd1fd04b7f2a38cbb138023fe849
tracer-ics2020 <tracer@njuics.org>
2020-10-27 19:35:09 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 90e6e32c87544185b84cdedaabcd7254     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:35:09 up 10:15,  1 user,  load average: 0.64, 0.53, 0.39

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,52 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  return 0;
+  va_list arg;
+  int cnt=0;
+  va_start(arg,fmt);
+  for(size_t i=0;fmt[i]!='\0';i++){
+    if(fmt[i]!='%'){
+      putch(fmt[i]);
+      cnt++;
+    }
+    else{
+      i++;
+      switch(fmt[i]){
+        case 'd':{
+          int tmp=va_arg(arg,int);
+          if(tmp<0){
+            putch('-');
+            cnt++;
+            tmp=(unsigned)(-tmp);
+          }
+          else{
+            tmp=(unsigned)tmp;
+          }
+          char digit[15];//from low to high
+          int pos=0;
+          do{
+            digit[pos++]=tmp%10+'0';
+            tmp/=10; 
+          }while(tmp!=0);
+          while(pos>0){
+            putch(digit[--pos]);
+            cnt++;
+          }
+          break;
+        }
+        case 's':{
+          char *tmp=va_arg(arg,char *);
+          for(size_t i=0;*(tmp+i)!='\0';i++){
+            putch(*(tmp+i));
+            cnt++;
+          }
+          break;
+        }
+      }
+    }
+  }
+   va_end(arg);
+  return cnt;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
