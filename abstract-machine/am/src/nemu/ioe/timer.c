3b4a3ba3336abe083aa669e4a709059397b68641
tracer-ics2020 <tracer@njuics.org>
2020-10-29 20:52:04 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 98ca5638ccc04522999224078a6e6255     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  20:52:04 up 18 min,  1 user,  load average: 0.33, 0.32, 0.27

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -2,19 +2,19 @@
 #include <nemu.h>
 
 //#include<stdio.h>
-// uint32_t boot_time_s;
-// uint32_t boot_time_us;
+uint32_t boot_time_s;
+uint32_t boot_time_us;
 uint32_t now_s;
 uint32_t now_us;
 void __am_timer_init() {
-  // boot_time_us=inl(RTC_ADDR);
-  // boot_time_s=inl(RTC_ADDR+4);
+  boot_time_us=inl(RTC_ADDR);
+  boot_time_s=inl(RTC_ADDR+4);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   now_us=inl(RTC_ADDR);
   now_s=inl(RTC_ADDR+4);
-  uptime->us = (now_s/*-boot_time_s*/) * 1000000 + (now_us/*-boot_time_us*/ + 500);
+  uptime->us = (now_s-boot_time_s) * 1000000 + (now_us-boot_time_us + 500);
 
   //printf("%d\n",now);
   // if(now<last){
