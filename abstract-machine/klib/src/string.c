e906300413a08cc1d37ac4be6e5e1c63e24f29ed
tracer-ics2020 <tracer@njuics.org>
2020-10-30 11:56:09 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 98ca5638ccc04522999224078a6e6255     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:56:09 up  2:37,  1 user,  load average: 0.12, 0.34, 0.37

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -16,9 +16,10 @@ size_t strlen(const char *s) {
 char *strcpy(char* dst,const char* src) {
     char *dest=(char*)dst;
   const char *srcc=(char*)src;
-  do{
+  while(*srcc!='\0'){
     *dest++=*srcc++;
-  }while(*srcc!='\0');
+  }
+  *dest='\0';
   return dst;
 }
 
@@ -44,25 +45,25 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  size_t i;
-  for(i=0;i<strlen(s1);i++){
-    if(*(s1+i)<*(s2+i)){
-      return -1;
-    }
-    else if(*(s1+i)>*(s2+i)){
-      return 1;
-    }
-  }
-  if(*(s2+i)!='\0'){
-    return -1;
-  }
-  return 0;
-  // size_t i=0,j=0;
-  // while(i==j&&s1[i]!='\0'&&s2[j]!='\0'){
-  //   i++;
-  //   j++;
+  // size_t i;
+  // for(i=0;i<strlen(s1);i++){
+  //   if(*(s1+i)<*(s2+i)){
+  //     return -1;
+  //   }
+  //   else if(*(s1+i)>*(s2+i)){
+  //     return 1;
+  //   }
   // }
-  // return s1[i]-s2[j];
+  // if(*(s2+i)!='\0'){
+  //   return -1;
+  // }
+  // return 0;
+  size_t i=0,j=0;
+  while(i==j&&s1[i]!='\0'&&s2[j]!='\0'){
+    i++;
+    j++;
+  }
+  return s1[i]-s2[j];
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
