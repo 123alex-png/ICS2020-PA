327471c0858ac5c2e5b4104f419fd446b8e7dd1f
tracer-ics2020 <tracer@njuics.org>
2020-10-30 22:11:10 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 98ca5638ccc04522999224078a6e6255     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:11:10 up  4:04,  1 user,  load average: 0.34, 0.21, 0.14

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -4,6 +4,6 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keycode=inl(KBD_ADDR)&0x7fff;
+  kbd->keycode=inl(KBD_ADDR);
   kbd->keydown=(inl(KBD_ADDR)&0x8000)?true:false;
 }
