a5a7d6bcba3a7999ea8f45b8302b073c7004f34c
tracer-ics2020 <tracer@njuics.org>
2020-10-31 15:32:21 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 98ca5638ccc04522999224078a6e6255     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:32:21 up 12:04,  1 user,  load average: 0.47, 0.58, 0.37

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -6,8 +6,8 @@
 void __am_gpu_init() {
   int i;
   uint32_t t=inl(VGACTL_ADDR);
-  int w = (t&0xffff);  // TODO: get the correct width
-  int h = (t&0xffff);  // TODO: get the correct height
+  int w = (t>>16);  // TODO: get the correct width
+  int h = (t>>16);  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
