25632551d5bd1b625686e736ea60f7bcbf3ec8f1
tracer-ics2020 <tracer@njuics.org>
2020-10-31 15:26:49 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 98ca5638ccc04522999224078a6e6255     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:26:49 up 11:59,  1 user,  load average: 0.11, 0.22, 0.19

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -4,13 +4,13 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int i;
-  // uint32_t t=inl(VGACTL_ADDR);
-  // int w = t>>16;  // TODO: get the correct width
-  // int h = t&0xffff;  // TODO: get the correct height
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);
+  int i;
+  uint32_t t=inl(VGACTL_ADDR);
+  int w = (t>>16)/2;  // TODO: get the correct width
+  int h = (t&0xffff)/2;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
