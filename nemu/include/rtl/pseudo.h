5db1dd0cf05f0a38bf297918c2cc96bb5e6bd655
tracer-ics2020 <tracer@njuics.org>
2020-11-03 22:21:48 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 773bd278e16a4796a07681ad0f5b3c7f     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:21:48 up 33 min,  1 user,  load average: 0.27, 0.74, 0.83

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -7,7 +7,7 @@
 
 /* RTL pseudo instructions */
 
-static inline def_rtl(li, rtlreg_t* dest, const rtlreg_t imm) {
+inline def_rtl(li, rtlreg_t* dest, const rtlreg_t imm) {
   rtl_addi(s, dest, rz, imm);
 }
 
