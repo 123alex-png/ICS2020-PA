2bf0a9d12c6a306b8130afda711c4fc9e1525bd9
tracer-ics2020 <tracer@njuics.org>
2020-11-22 21:33:46 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: a6ceba1bbf9d418facfac6aef39a5bfc     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:33:46 up 10:47,  1 user,  load average: 0.26, 0.42, 0.47

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -4,7 +4,7 @@ static inline def_EHelper(lidt) {
   // if(s->isa.is_operand_size_16){
   //   cpu.idtr;
   // }
-  rtl_lm(s,&(cpu.idtr.base),ddest,0,48);
+  rtl_lm(s,&(cpu.idtr.base),dsrc1,0,48);
   print_asm_template1(lidt);
 }
 
