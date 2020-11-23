9baac3bfaa22a919591e6d45fc7a2b1af50ef6f3
tracer-ics2020 <tracer@njuics.org>
2020-11-23 09:56:00 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: a6ceba1bbf9d418facfac6aef39a5bfc     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:56:01 up 13:07,  1 user,  load average: 0.41, 0.45, 0.40

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -6,8 +6,8 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   rtlreg_t addr=cpu.idtr.base+8*NO;
-  rtlreg_t high=vaddr_read(addr,4);
-  rtlreg_t low=vaddr_read(addr+4,4);
+  rtlreg_t low=vaddr_read(addr,4);
+  rtlreg_t high=vaddr_read(addr+4,4);
   rtlreg_t entry=(high&0xffff0000)+(low&0xffff);
   rtl_push(s,(rtlreg_t *)&(cpu.eflags));
   rtl_push(s,(rtlreg_t *)&(cpu.cs));
