b8fe6ea0df7a8694aadc131759bf45724ad938b2
tracer-ics2020 <tracer@njuics.org>
2020-11-25 17:19:54 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:19:54 up 40 min,  1 user,  load average: 0.98, 0.82, 0.57

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,24 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   TODO();
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, 52);
+  for(int i=0;i<ehdr.e_phnum;i++){
+    Elf_Phdr phdr;
+    ramdisk_read(&phdr,ehdr.e_phoff+i*ehdr.e_phentsize,ehdr.e_phentsize);
+    if(phdr.p_type==PT_LOAD){
+      memcpy((void *)phdr.p_vaddr,(void *)(ramdisk_start+phdr.p_offset),phdr.p_filesz);
+      memset((void *)(phdr.p_vaddr+phdr.p_filesz),0,phdr.p_memsz-phdr.p_filesz);
+    }
+  }
   return 0;
 }
 
