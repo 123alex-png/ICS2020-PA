31634bfffc696cd7b3f69b2d0fb46a340e69b6a8
tracer-ics2020 <tracer@njuics.org>
2020-11-26 10:36:29 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:36:30 up  9:35,  1 user,  load average: 0.32, 0.38, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,24 +1,38 @@
 #include <common.h>
 #include "syscall.h"
 
-uintptr_t sys_yield(){
+int sys_yield(){
   yield();
   return 0;
 }
 
-uintptr_t sys_exit(){
+int sys_exit(){
   halt(0);
   return 0;
 }
 
+int sys_write(int fd, void *buf, size_t count){
+  switch(fd){
+    case 1:
+    case 2:{
+      for(size_t i=0;i<count;i++){
+        putch(*(char *)(buf+i));
+      }
+    }
+  }
+  return count;
+}
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   switch (a[0]) {
     case SYS_exit:sys_exit();break;
     case SYS_yield:sys_yield();break;
+    case SYS_write:sys_write((int)a[1],(void *)a[2],(size_t)a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
