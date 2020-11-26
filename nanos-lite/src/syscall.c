87dcd0308fedddf1a8b1699d3719c1eb9cff8198
tracer-ics2020 <tracer@njuics.org>
2020-11-26 10:12:46 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:12:46 up  9:11,  1 user,  load average: 0.91, 0.52, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,19 @@
 #include <common.h>
 #include "syscall.h"
+
+uintptr_t sys_yield(){
+  yield();
+  return 0;
+}
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case 1:sys_yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+
