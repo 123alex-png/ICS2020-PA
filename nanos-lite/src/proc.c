23d2bcfa52f3f4cb75723a4fe722287d77f629cf
tracer-ics2020 <tracer@njuics.org>
2020-11-30 21:58:45 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:58:46 up 22:29,  1 user,  load average: 0.64, 0.59, 0.55

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL,"/home/quli/ics2020/navy-apps/tests/hello/build/hello-x86");
+  naive_uload(NULL,"/bin/hello");
 }
 
 Context* schedule(Context *prev) {
