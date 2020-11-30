6e09239def7cadbc0835184747f5b43b573e4fd8
tracer-ics2020 <tracer@njuics.org>
2020-12-01 04:29:30 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:29:30 up 1 day,  4:31,  1 user,  load average: 0.83, 0.54, 0.52

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -61,7 +61,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_write, fd, buf, count)
+  _syscall_(SYS_write, fd, buf, count);
   return count;//0;
 }
 
