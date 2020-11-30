afcc4099a051ec25a0f48a52f4d1d35e6aa6328d
tracer-ics2020 <tracer@njuics.org>
2020-12-01 05:37:07 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:37:07 up 1 day,  5:39,  1 user,  load average: 0.73, 0.49, 0.39

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -76,6 +76,10 @@ size_t fs_read(int fd, void *buf, size_t len){
 }
 
 size_t fs_write(int fd, const void *buf, size_t len){
+  return file_table[fd].write(buf,0,len);
+}
+
+size_t _write(int fd, const void *buf, size_t len){
   size_t offset=file_table[fd].disk_offset+open_offset[fd];
   open_offset[fd]+=len;
   return ramdisk_write(buf,offset,len);
