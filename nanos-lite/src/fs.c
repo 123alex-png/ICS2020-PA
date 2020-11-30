97b34924b613a8c760be6bfe73b77a88bb0d7cc3
tracer-ics2020 <tracer@njuics.org>
2020-12-01 05:06:35 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:06:35 up 1 day,  5:08,  1 user,  load average: 0.30, 0.28, 0.33

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,7 +82,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence){
-  printf("cur=%d,offset=%d,whence = %d, size = %d\n",open_offset[fd],offset,whence,file_table[fd].size);
+  
   switch(whence){
     case SEEK_SET:{
       if(offset>0&&offset>file_table[fd].size){
@@ -91,6 +91,7 @@ off_t fs_lseek(int fd, off_t offset, int whence){
         return (off_t)-1;
       }
       open_offset[fd]=offset;
+      printf("cur=%d,offset=%d,whence = %d, size = %d\n",open_offset[fd],offset,whence,file_table[fd].size);
       break;
     }
     case SEEK_CUR:{
