ff41b21e3bf8f1fa34e7432491a2beb97c5feada
tracer-ics2020 <tracer@njuics.org>
2020-12-01 03:31:41 +0800

    >  gdb 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:31:42 up 1 day,  3:34,  1 user,  load average: 1.01, 0.59, 0.44

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -57,13 +57,12 @@ void _exit(int status) {
 
 int _open(const char *path, int flags, mode_t mode) {
   //_exit(SYS_open);
-  _syscall_(SYS_open,path,0,0);
-  return 0;
+  
+  return _syscall_(SYS_open,path,0,0);
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_write, fd, buf, count);
-  return 0;
+  return _syscall_(SYS_write, fd, buf, count);
 }
 
 static intptr_t prog_break;
@@ -80,21 +79,19 @@ void *_sbrk(intptr_t increment) {
 
 
 int _read(int fd, void *buf, size_t count) {
-  _syscall_(SYS_read,fd,buf,count);
+  
   //_exit(SYS_read);
-  return 0;
+  return _syscall_(SYS_read,fd,buf,count);
 }
 
 int _close(int fd) {
   //_exit(SYS_close);
-  _syscall_(SYS_close,fd,0,0);
-  return 0;
+  return _syscall_(SYS_close,fd,0,0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
   //_exit(SYS_lseek);
-  _syscall_(SYS_lseek,fd,offset,whence);
-  return 0;
+  return _syscall_(SYS_lseek,fd,offset,whence);
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
