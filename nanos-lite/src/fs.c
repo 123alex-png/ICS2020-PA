85eed73e34aa644af48967d49755229a321d33d4
tracer-ics2020 <tracer@njuics.org>
2020-12-02 16:40:17 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:40:17 up 1 day,  6:09,  1 user,  load average: 0.30, 0.52, 0.43

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -48,7 +48,7 @@ size_t getoffset(int fd){
 }
 
 int fs_open(const char *pathname, int flags, int mode){
-  for(int i=3;i<FT_SIZE;i++){
+  for(int i=3;i<25;i++){
     if(!strcmp(pathname,file_table[i].name)){
       return i;
     }
