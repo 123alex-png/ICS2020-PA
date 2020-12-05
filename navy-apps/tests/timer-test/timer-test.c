f42c8356ae0dfeaf8c5744c09a3b96abb64b3b35
tracer-ics2020 <tracer@njuics.org>
2020-12-05 23:26:22 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:26:22 up 1 day, 11:30,  1 user,  load average: 1.31, 1.02, 0.75

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -3,11 +3,13 @@
 
 int main(){
     //int cur=1000;
+    int cnt=0;
     while(1){
         //gettimeofday(&tv, NULL);
         uint32_t time=NDL_GetTicks();
         printf("%dms\n", time);
         sleep(1);
+        assert(0);
     }
 }
 
