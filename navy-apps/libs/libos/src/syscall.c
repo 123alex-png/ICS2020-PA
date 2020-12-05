ec12e572f9d3051aa62a00954b95bdb57187b421
tracer-ics2020 <tracer@njuics.org>
2020-12-05 23:37:59 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: b246d66e202e4e9f8a7f6118cee13e4e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:37:59 up 1 day, 11:41,  1 user,  load average: 0.72, 0.51, 0.61

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -180,7 +180,7 @@ unsigned int sleep(unsigned int seconds) {
   while(1){
     struct timeval tv;
     _gettimeofday(&tv, NULL);
-    printf("sec=%d,usec=%d\n",tv.tv_sec, tv.tv_usec);
+    printf("sec=%d",tv.tv_sec);
     if(tv.tv_sec-init.tv_sec>=seconds){
       //assert(0);
       //assert(last<10);
