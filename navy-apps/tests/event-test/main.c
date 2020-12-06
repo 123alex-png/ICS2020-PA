c2f3c12091c31d7cf8d9e2d7e28c6e9aa44028c0
tracer-ics2020 <tracer@njuics.org>
2020-12-06 20:16:11 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 2f9674364da245efaee331e7aa4fcf87     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  20:16:11 up  4:41,  1 user,  load average: 0.99, 0.75, 0.65

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -5,10 +5,8 @@ int main() {
   NDL_Init(0);
   while (1) {
     char buf[64];
-    //printf("%s\n", buf);
     if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("receive event: %s\n", buf);
-      //assert(0);
     }
   }
   return 0;
