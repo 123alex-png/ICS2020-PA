8aa5f21886e5c0fc975925b2147ce596700539bd
tracer-ics2020 <tracer@njuics.org>
2020-12-07 22:39:02 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 1660c4d6e8fa470091b8f313be607f92     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:39:02 up  6:52,  1 user,  load average: 0.57, 0.54, 0.48

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -15,6 +15,10 @@ int _read(int fd, void *buf, size_t count);
 int _write(int fd, void *buf, size_t count);
 off_t _lseek(int fd, off_t offset, int whence);
 
+FILE *fp;
+
+
+
 uint32_t NDL_GetTicks() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
@@ -28,8 +32,6 @@ int NDL_PollEvent(char *buf, int len) {
     // if(ret!=0){
     //   return 1;
     // }
-    
-    FILE *fp=fopen("/dev/events", "r");
     
     int ret=fread(buf, 1, len, fp);
     // fscanf(fp,"%s", buf+3);
@@ -110,6 +112,7 @@ int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
+  fp=fopen("/dev/events", "r");
   return 0;
 }
 
