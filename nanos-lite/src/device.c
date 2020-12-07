55e9863655a58b512beebe21b5e7e45c721b2452
tracer-ics2020 <tracer@njuics.org>
2020-12-07 16:18:51 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 1660c4d6e8fa470091b8f313be607f92     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:18:51 up 58 min,  1 user,  load average: 1.31, 0.59, 0.35

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -44,7 +44,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T cfg;
   ioe_read(AM_GPU_CONFIG, &cfg);
   size_t info =(cfg.width << 4) + cfg.height;
-  printf("info = %u\n", info);
+  printf("info = %d\n", info);
   char tmp[50];
   sprintf(tmp, "%d", info);
   strncpy(buf, tmp, len);
