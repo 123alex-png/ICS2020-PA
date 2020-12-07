ac94ff36809d4afafb6b75ae895be5ee773de865
tracer-ics2020 <tracer@njuics.org>
2020-12-07 17:28:02 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 1660c4d6e8fa470091b8f313be607f92     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:28:03 up  2:07,  1 user,  load average: 0.99, 0.83, 0.70

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -22,17 +22,14 @@ struct BitmapHeader {
 void* BMP_Load(const char *filename, int *width, int *height) {
   FILE *fp = fopen(filename, "r");
   if (!fp) return NULL;
-
   struct BitmapHeader hdr;
   assert(sizeof(hdr) == 54);
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
-
   if (hdr.bitcount != 24) return NULL;
   if (hdr.compression != 0) return NULL;
   int w = hdr.width;
   int h = hdr.height;
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
-
   int line_off = (w * 3 + 3) & ~0x3;
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
