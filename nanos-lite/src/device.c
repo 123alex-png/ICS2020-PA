55f96d95587d27b38062fa4fb3b55921cb79e738
tracer-ics2020 <tracer@njuics.org>
2020-12-07 20:37:48 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 1660c4d6e8fa470091b8f313be607f92     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  20:37:48 up  4:51,  1 user,  load average: 0.73, 0.57, 0.50

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,7 +20,7 @@ static const char *names[] = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  for(size_t i=0;i<len;i++){
+  for(size_t i=0;i<len&& *(char *)(buf+i)!='\0';i++){
         putch(*(char *)(buf+i));
       }
   return len;
