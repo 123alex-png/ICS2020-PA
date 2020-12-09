0f22b939d15e918c042bdc944242d257cc2ee6fc
tracer-ics2020 <tracer@njuics.org>
2020-12-09 22:03:36 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: bdfdf1de9aea4874acb20368d54cd7ea     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:03:37 up 47 min,  1 user,  load average: 1.08, 0.81, 0.70

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -14,11 +14,27 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  SDL_WaitEvent(ev);
-  if(ev->key.keysym.sym == SDLK_NONE){
-    // re
+  char buf[64];
+  ev->type = SDL_KEYUP;
+  if(NDL_PollEvent(buf, sizeof(buf))){
+    printf("receive event: %s\n", buf);
+    if(buf[1]=='u'){
+      ev->type = SDL_KEYUP;
+    }
+    else{
+      ev->type = SDL_KEYDOWN;
+    }
+    char *key = buf+3;
+    //printf("key = %s, %d\n", key, strlen(key));
+    for(int i = 0; i < 83; i++){
+      //printf("%s, %d\n", keyname[i], strlen(keyname[i]));
+      if(!strcmp(key, keyname[i])){
+        ev->key.keysym.sym = i;
+        return 0;
+      }
+    }
   }
-  return 0;
+  return -1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
