d7ce574cc553a886bc3c40e97b4ad44cac814bb7
tracer-ics2020 <tracer@njuics.org>
2020-12-13 18:40:13 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: bdfdf1de9aea4874acb20368d54cd7ea     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:40:13 up 23:12,  1 user,  load average: 0.21, 0.33, 0.37

--- nemu/src/isa/x86/exec2/control.h
+++ nemu/src/isa/x86/exec2/control.h
@@ -12,26 +12,27 @@ static inline def_EHelper(jcc) {
   uint32_t cc = s->opcode & 0xf;
   rtl_setcc(s, s0, cc);
   rtl_jrelop(s, RELOP_NE, s0, rz, s->jmp_pc);
-
+ /* printf("%x\n",s->jmp_pc);*/
   print_asm("j%s %x", get_cc_name(cc), s->jmp_pc);
 }
 
 static inline def_EHelper(jmp_rm) {
   rtl_jr(s, ddest);
-
   print_asm("jmp *%s", id_dest->str);
 }
 
 static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
-  rtl_push(s,&(s->seq_pc));
+  rtl_push(s,&s->seq_pc);
   rtl_j(s,s->jmp_pc);
   print_asm("call %x", s->jmp_pc);
 }
 
 static inline def_EHelper(ret) {
-  rtl_pop(s,s0);
-  rtl_j(s,*s0);
+ rtl_pop(s,s0);
+ /*printf("%x\n",*s0);*/
+ rtl_j(s,*s0);
+
   print_asm("ret");
 }
 
@@ -41,9 +42,17 @@ static inline def_EHelper(ret_imm) {
   print_asm("ret %s", id_dest->str);
 }
 
-static inline def_EHelper(call_rm) {
-  rtl_push(s,&(s->seq_pc));
-  rtl_li(s,s0,*ddest);
-  rtl_j(s,*s0);
+static inline def_EHelper(call_rm) {//incompleted
+  
+   if(*ddest==0){
+     printf("%x\n",cpu.pc);
+     printf("%x\n",cpu.edx);
+   }
+   /*if(cpu.pc==0x12cbd8){
+     printf("%x\n",*ddest);
+     printf("%x\n",cpu.edx);
+   }*/
+  rtl_push(s,&s->seq_pc);
+   rtl_jr(s,ddest);
   print_asm("call *%s", id_dest->str);
 }
