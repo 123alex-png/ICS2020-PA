cb21358c5c7c6b38886d325ecf3942c7106ce01b
tracer-ics2020 <tracer@njuics.org>
2020-12-13 17:02:35 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: bdfdf1de9aea4874acb20368d54cd7ea     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:02:36 up 21:34,  1 user,  load average: 0.38, 0.37, 0.36

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -82,7 +82,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
     uint32_t *pixels = (uint32_t *)dst->pixels;
     for(int i = 0; i < h; i ++){
       for(int j = 0;j < w; j ++){
-        pixels[w * i + j] = color;
+        pixels[dst->w * i + j] = color;
         // printf("%d\n", w * i + j);
       }
     }
