c126bb7798e43e03780ab3b38cb322aaab157e5f
tracer-ics2020 <tracer@njuics.org>
2020-12-13 21:25:59 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 037595519ca847dcb37dcb391c72ee2f     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:26:00 up  2:18,  1 user,  load average: 0.64, 0.37, 0.30

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -318,14 +318,11 @@ vaddr_t isa_exec_once() {
   s.is_jmp = 0;
   s.isa = (ISADecodeInfo) { 0 };
   s.seq_pc = cpu.pc;
-  if(cpu.pc==0x41c1da/*0x41be70*/){
+  if(cpu.pc==0x41bd90/*0x41be70*/){
     printf("cpu.ax = %x\n",cpu.ax);
     assert(0);
   }
   fetch_decode_exec(&s);
   update_pc(&s);
-  if(++cnt > 100000000){
-    printf("%ld\n",cnt);
-  }
   return s.seq_pc;
 }
