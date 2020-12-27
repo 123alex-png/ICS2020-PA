1df253d5aaefdd27a48cfbccb107b3364fd5f563
tracer-ics2020 <tracer@njuics.org>
2020-12-27 13:58:40 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:58:40 up  1:15,  1 user,  load average: 0.39, 0.37, 0.37

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@ Context* schedule(Context *prev);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_SYSCALL: do_syscall(c);break;
-    case EVENT_YIELD: schedule(c);break;
+    case EVENT_YIELD: c = schedule(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
