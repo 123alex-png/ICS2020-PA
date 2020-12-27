7c318e2f0b929edd975acf215ff3edc86992309f
tracer-ics2020 <tracer@njuics.org>
2020-12-27 15:31:23 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:31:23 up  2:48,  1 user,  load average: 0.46, 0.48, 0.38

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,7 +13,7 @@ void switch_boot_pcb() {
 }
 
 void hello_fun(void *arg) {
-  assert(arg);
+  // assert(arg);
   int j = 1;
   while (1) {
     Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
