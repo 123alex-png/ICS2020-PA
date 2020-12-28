00f849458dea0e2cf01013d04a1a8a22d0837224
tracer-ics2020 <tracer@njuics.org>
2020-12-28 23:02:18 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:02:19 up 17:35,  1 user,  load average: 0.36, 0.67, 0.71

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -61,8 +61,34 @@ void context_kload(PCB *pcb, void *entry, void *arg){
 
 void context_uload(PCB *pcb, char *filename, char *const argv[], char *const envp[]){
   uintptr_t entry = loader(pcb, filename);
-  Area stack;
-  stack.start = pcb->stack;
-  stack.end = stack.start + sizeof(pcb->stack);
-  pcb->cp = ucontext(&(pcb->as), stack, (void *)entry);
+  Area ustack;
+  ustack.start = pcb->stack;
+  ustack.end = ustack.start + sizeof(pcb->stack);
+  pcb->cp = ucontext(&(pcb->as), ustack, (void *)entry);
+  Context *c = pcb->cp;
+  if(argv != NULL){
+    intptr_t *argp = ustack.end - 0x80;
+    printf("argp = %p\n", argp);
+    int argc = 1;
+    char *last= (char *)argp + 0x30;
+    for(; /*argv[argc]!=NULL*/argc<=1; argc++){
+      argp[argc] = (intptr_t)last;
+      printf("argp[%d] = %p\n", argc, argp[argc]);
+      last += strlen(argv[argc-1]);
+  }
+    argp[argc] = 0;
+    --argc;
+    argp[0] = argc;
+    c -> GPRx = (uintptr_t)argp;
+    char *end = (char *)argp + 0x30;//至多可放12个参数，所有参数长度和至多80字节
+    for(int i = 0; i < argc; i++){
+      for(int j = 0; j < strlen(argv[i]); j++){
+        *end++ = argv[i][j];
+        printf("end = %p, *end = %c\n", end - 1, *(end-1));
+      }
+    }
+  }
+  else{
+    c -> GPRx = 0;
+  }
 }
