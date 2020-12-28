c44bb85d86ac3da7aac77266820f8880d156f588
tracer-ics2020 <tracer@njuics.org>
2020-12-28 22:08:57 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:08:57 up 16:53,  1 user,  load average: 1.04, 0.67, 0.62

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -8,7 +8,9 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main(int      argc, char    *argv[]) {
-  volatile char tmp = argv[0];
+  // volatile char tmp = argv[0];
+  // printf("%c\n")
+  putchar(argv[0]);
   while(1);
   return _syscall_(SYS_yield, 0, 0, 0);
 }
