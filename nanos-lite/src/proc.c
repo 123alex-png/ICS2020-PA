487e33821944e2e1ea5dab45f932a22460fa12d1
tracer-ics2020 <tracer@njuics.org>
2020-12-28 20:45:55 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  20:45:55 up 15:30,  1 user,  load average: 1.43, 0.86, 0.68

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  // context_kload(&pcb[0], (void *)hello_fun, (void *)12345);
+  context_kload(&pcb[0], (void *)hello_fun, (void *)12345);
   // context_kload(&pcb[1], (void *)hello_fun, (void *)52314);
   char *args[] ={"--ski"};
   context_uload(&pcb[1], "/bin/pal", args, NULL);
