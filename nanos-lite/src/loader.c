18d63010f57d1b3c61b336661cf42c99caf22d44
tracer-ics2020 <tracer@njuics.org>
2020-12-29 18:22:53 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:22:53 up 20:53,  1 user,  load average: 0.89, 0.62, 0.61

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -80,7 +80,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   }
     argp[argc] = 0;
     --argc;
-    printf("argc = %d", argc);
+    // printf("argc = %d", argc);
     argp[0] = argc;
     c -> GPRx = (uintptr_t)argp;
     char *end = (char *)argp + 0x100;//至多可放12个参数，所有参数长度和至多80字节
@@ -91,6 +91,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
       }
       *end++ = '\0';
     }
+    printf("argv[1] = %s", argv[1]);
   }
   else{
     c -> GPRx = 0;
