4087ab623dce737b417f7641d2b8f2ede2dd147f
tracer-ics2020 <tracer@njuics.org>
2020-12-29 17:11:17 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:11:18 up 19:41,  1 user,  load average: 0.54, 0.65, 0.52

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -25,8 +25,7 @@ int mm_brk(uintptr_t brk) {
 }
 
 void init_mm() {
-  pf = (void *)ROUNDUP(heap.start, PGSIZE);
+  // pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
-
   vme_init(pg_alloc, free_page);
 }
