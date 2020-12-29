12ca9c6262614de359e42e81c558c66cc8c5efbd
tracer-ics2020 <tracer@njuics.org>
2020-12-29 19:40:15 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:40:15 up 22:10,  1 user,  load average: 1.03, 0.67, 0.58

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -110,7 +110,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
       argc++;
     }
     printf("%d\n", argc);
-    uintptr_t *p = (void *)(argp - argc - 1);
+    uintptr_t *p = (void *)(argp) - argc - 1;
     printf("p=%p\n", p);
     for(int i = 0; i < argc; i++){
       p[i+1] = (uintptr_t)ptr[argc - i]; 
