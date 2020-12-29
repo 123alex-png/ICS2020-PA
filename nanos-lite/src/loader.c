fb224a809f625fa7a5a4098bce0a1923e9d9b619
tracer-ics2020 <tracer@njuics.org>
2020-12-29 19:13:46 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:13:46 up 21:44,  1 user,  load average: 0.74, 0.76, 0.61

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -73,10 +73,10 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   pcb->cp = ucontext(&(pcb->as), ustack, (void *)entry);
   Context *c = pcb->cp;
   if(argv != NULL){
-    intptr_t *argp = ustack.end - sizeof(Context) - 0x30;
+    intptr_t *argp = ustack.end - sizeof(Context) - 0x300;
     // printf("argp = %p\n", argp);
     int argc = 1;
-    char *last= (char *)argp + 0x10;
+    char *last= (char *)argp + 0x30;
     for(; /*argv[argc]!=NULL*/argv[argc-1]!=NULL; argc++){
       argp[argc] = (intptr_t)last;
       // printf("argp[%d] = %p\n", argc, argp[argc]);
@@ -87,7 +87,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     // printf("argc = %d", argc);
     argp[0] = argc;
     c -> GPRx = (uintptr_t)argp;
-    char *end = (char *)argp + 0x10;//至多可放12个参数，所有参数长度和至多80字节
+    char *end = (char *)argp + 0x30;//至多可放12个参数，所有参数长度和至多80字节
     for(int i = 0; i < argc; i++){
       for(int j = 0; argv[i][j]!='\0'; j++){
         *end++ = argv[i][j];
