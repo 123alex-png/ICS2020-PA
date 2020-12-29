ef8fe936067dc9b971eccf8ea6af0ad554e08961
tracer-ics2020 <tracer@njuics.org>
2020-12-29 17:12:34 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:12:34 up 19:43,  1 user,  load average: 0.66, 0.65, 0.53

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -11,13 +11,13 @@ void* new_page(size_t nr_page) {
   return p;
 }
 
-static void* pg_alloc(int n) {
-  return NULL;
-}
+// static void* pg_alloc(int n) {
+//   return NULL;
+// }
 
-void free_page(void *p) {
-  panic("not implement yet");
-}
+// void free_page(void *p) {
+//   panic("not implement yet");
+// }
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
@@ -27,5 +27,5 @@ int mm_brk(uintptr_t brk) {
 void init_mm() {
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
-  vme_init(pg_alloc, free_page);
+  // vme_init(pg_alloc, free_page);
 }
