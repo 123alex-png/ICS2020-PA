390beeed74bd689d72bf48149731fe37e98aa7d1
tracer-ics2020 <tracer@njuics.org>
2020-12-29 19:11:34 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:11:34 up 21:42,  1 user,  load average: 0.81, 0.67, 0.55

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -4,12 +4,11 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  printf("argc = %d, argv[1] = %s\n", argc, argv[1]);
+  printf("argv[1] = %s\n", argv[1]);
   int n = (argc >= 2 ? atoi(argv[1]) : 1);
   printf("%s: argv[1] = %d\n", argv[0], n);
   char buf[16];
   sprintf(buf, "%d", n + 1);
-  printf("buf = %s\n", buf);
   execl(argv[0], argv[0], buf, NULL);
   return 0;
 }
