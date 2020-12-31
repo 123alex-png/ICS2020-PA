eb9e2436b3b6c0b08e272990ee595469fdf5fa80
tracer-ics2020 <tracer@njuics.org>
2020-12-31 22:18:46 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:18:46 up 1 day,  9:07,  1 user,  load average: 0.43, 0.48, 0.53

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -76,7 +76,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   printf("pgdir = %p, pgtab = %p\n", pgdir, pgtab);
   size_t pgtab_index = ((uint32_t)va >> 12) & 0x3ff;//中间10位
   if((pgtab[pgtab_index] & PTE_P) == 1){//是否存在这种情况？？？
-    panic("PTE_P of pgtab has been 1");
+    // panic("PTE_P of pgtab has been 1");
   }
   pgtab[pgtab_index] = ((uintptr_t)pa & ~0xfff) | PTE_P;
 }
