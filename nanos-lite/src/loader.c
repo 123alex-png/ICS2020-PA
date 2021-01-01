59022ab01d4f0d86bf94bd2fcfad95b3d4bea18b
tracer-ics2020 <tracer@njuics.org>
2021-01-01 23:46:51 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:46:51 up 1 day, 16:39,  1 user,  load average: 0.50, 0.42, 0.47

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -64,10 +64,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       fs_read(fd,&phdr,ehdr.e_phentsize);
     if(phdr.p_type==PT_LOAD){
       fs_lseek(fd,phdr.p_offset,SEEK_SET);
-      // fs_read(fd,(void *)phdr.p_vaddr,phdr.p_filesz);
+      fs_read(fd,(void *)phdr.p_vaddr,phdr.p_filesz);
       memset((void *)(phdr.p_vaddr+phdr.p_filesz),0,phdr.p_memsz-phdr.p_filesz);
       
-      page_load(fd, pcb, phdr.p_vaddr, phdr.p_filesz, phdr.p_memsz);//以页为单位加载
+      // page_load(fd, pcb, phdr.p_vaddr, phdr.p_filesz, phdr.p_memsz);//以页为单位加载
     }
   }
   return ehdr.e_entry;
