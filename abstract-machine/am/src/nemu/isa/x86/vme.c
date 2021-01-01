e7fe9d444bf247345b2259586b31444bf4c3cb8a
tracer-ics2020 <tracer@njuics.org>
2021-01-02 01:06:29 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  01:06:29 up 1 day, 17:58,  1 user,  load average: 0.74, 0.52, 0.38

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -22,8 +22,9 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
-    printf("va: %p\n",va);
+    
     for (; va < segments[i].end; va += PGSIZE) {
+      printf("va: %p\n",va);
       map(&kas, va, va, 0);
     }
   }
