56f0bcd39218141437fe4a67a5f644987eb01d82
tracer-ics2020 <tracer@njuics.org>
2021-01-02 00:06:55 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  00:06:55 up 1 day, 16:59,  1 user,  load average: 0.85, 0.75, 0.68

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -29,9 +29,10 @@ word_t vaddr_mmu_read(vaddr_t addr, int len, int type) {
 }
 
 void vaddr_mmu_write(vaddr_t addr, uint32_t data, int len, int type){
+
   if (OFFSET(addr)+len <= PAGE_SIZE) {
     paddr_t pg_base = isa_mmu_translate(addr, type, len);
-    return paddr_write(pg_base, data, len);
+    paddr_write(pg_base, data, len);
   } else {
     assert(OFFSET(addr)+len <= PAGE_SIZE);
   }
