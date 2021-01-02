a793960ecdca7bc5bd7c7e8742101972f547e1ab
tracer-ics2020 <tracer@njuics.org>
2021-01-02 22:15:25 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9c150c3e689146a1805bb3ae3bb765c4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:15:25 up 1 day, 20:03,  1 user,  load average: 0.72, 0.71, 0.65

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -27,7 +27,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {//后2个参数的�
 }
 
 word_t vaddr_mmu_read(vaddr_t addr, int len, int type) {
-  printf("addr: %x\n",addr);
+  
   // return paddr_read(addr, len);
   word_t right_data = paddr_read(addr, len);
   if (OFFSET(addr)+len <= PAGE_SIZE) {
@@ -38,17 +38,22 @@ word_t vaddr_mmu_read(vaddr_t addr, int len, int type) {
   } else {
     size_t len1 = PAGE_SIZE - OFFSET(addr);
     size_t len2 = len - len1;
-    printf("len1: %ld, len2: %ld, len: %d\n",len1,len2,len);
+    
     uintptr_t paddr1 = isa_mmu_translate(addr, type, len);
     uintptr_t paddr2 = isa_mmu_translate(addr+len1, type, len);
-    printf("paddr1: %lx, paddr2: %lx\n", paddr1,paddr2);
+    
     word_t ret2 = (paddr_read(paddr2, 4));//<<(8*(4-len2)));
     word_t ret1 = ((paddr_read(paddr1, 4)));//>>8*(4-len1));
     
     word_t ret = ret1|ret2;
-    printf("ret1: %x, ret2: %x, read : %x\n", ret1, ret2, ret);
+    
+    if(ret != right_data){
+      printf("addr: %x\n",addr);
+      printf("len1: %ld, len2: %ld, len: %d\n",len1,len2,len);
+      printf("paddr1: %lx, paddr2: %lx\n", paddr1,paddr2);
+      printf("ret1: %x, ret2: %x, read : %x\n", ret1, ret2, ret);
     printf("ret: %x, right: %x\n", ret, right_data);
-    assert(ret == right_data);
+    }
     return ret;
 
   }
