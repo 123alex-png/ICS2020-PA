00ef28add6c089c323e4acd88e00294d495214e0
tracer-ics2020 <tracer@njuics.org>
2021-01-16 19:19:23 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: cb133aae3b1e46a8bc73c382c553a162     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:19:23 up  6:33,  1 user,  load average: 0.38, 0.45, 0.54

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -57,6 +57,7 @@ static void sh_handle_cmd(const char *cmd) {
       // printf("%d\n",args[1][0]);
       int i = args[1]==NULL?0:1;
       // assert(i==0);
+      printf("%d %d\n",args[i][strlen(args[i])-2],args[i][strlen(args[i])-1]);
       if(args[i][strlen(args[i])-2]>0x1f){//x86-nemu没有'\t'，故需特判
         args[i][strlen(args[i])-1]='\0';
         assert(0);
