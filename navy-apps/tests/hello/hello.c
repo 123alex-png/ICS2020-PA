42b5a12c42c20a7fc09a4a0b95df13619ea3d5e7
tracer-ics2020 <tracer@njuics.org>
2021-01-20 17:48:21 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:48:21 up 38 min,  1 user,  load average: 0.26, 0.47, 0.37

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,14 +3,14 @@
 
 int main() {
 	
-  write(1, "Hello World!\n", 13);while(1);
+  write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
   while (1) {
     j ++;
     if (j == 10000) {
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
+      j = 0;while(1);
     }
   }
   return 0;
