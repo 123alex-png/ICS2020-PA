79cf7c95bd3dc470db275d307ff673a5414deffc
tracer-ics2020 <tracer@njuics.org>
2021-01-20 18:56:37 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:56:37 up  1:46,  1 user,  load average: 0.39, 0.32, 0.28

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -82,12 +82,12 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   size_t pgtab_index = ((uint32_t)va >> 12) & 0x3ff;//中间10位
   // printf("pgdir_index = %d, pgtab_index = %d\n", pgdir_index, pgtab_index);
   // printf("pgdir = %p, pgtab = %p\n", pgdir, pgtab);
-  printf("pgtab[%d] = %p\n", pgtab_index, pgtab[pgtab_index]);
+  
   if((pgtab[pgtab_index] & PTE_P) == 1){//是否存在这种情况？？？
     printf("pgtab: %p\n", pgtab);
     panic("PTE_P of pgtab has been 1\n");
   }
-  pgtab[pgtab_index] = ((uintptr_t)pa & ~0xfff) | PTE_P;
+  pgtab[pgtab_index] = ((uintptr_t)pa & ~0xfff) | PTE_P;printf("pgtab[%d] = %p\n", pgtab_index, pgtab[pgtab_index]);
 }
 
 
