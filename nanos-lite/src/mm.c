c6d2333ae0e402c59929fe77138222937bdef813
tracer-ics2020 <tracer@njuics.org>
2021-01-20 18:02:44 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:02:44 up 52 min,  1 user,  load average: 0.28, 0.30, 0.33

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -24,16 +24,18 @@ void free_page(void *p) {
   panic("not implement yet");
 }
 
+
+
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
  
   // printf("brk: %p\n", (void *)brk);
   if(brk > current->max_brk){
     uintptr_t va = current->max_brk;
-    // if(va % PGSIZE != 0){
-    //   void *pa = new_page(1);
-    //   map(&(current->as), (void *)(ROUNDDOWN(va, PGSIZE)), pa, stdprot);
-    // }
+    if(va % PGSIZE != 0){
+      void *pa = new_page(1);
+      map(&(current->as), (void *)(ROUNDDOWN(va, PGSIZE)), pa, stdprot);
+    }
     va = ROUNDUP(va, PGSIZE);
     while(va < brk){
       void *pa = new_page(1);
