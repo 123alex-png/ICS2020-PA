db1c2b1b8448016809c0d13979f8cd7f01ca326b
tracer-ics2020 <tracer@njuics.org>
2021-01-20 13:34:57 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: cb133aae3b1e46a8bc73c382c553a162     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:34:57 up 20:48,  1 user,  load average: 0.89, 0.63, 0.51

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -58,19 +58,21 @@ void page_load(int fd, PCB *pcb, uintptr_t vaddr, uint32_t filesz, uint32_t mems
   memset((void *)(paddr+leftsz), 0, PGSIZE-leftsz);//该页剩余部分填0
   //清零memsz-filesz
   int left = memsz - filesz - (PGSIZE - leftsz);
-  uintptr_t start = vaddr+(i+1)*PGSIZE;
-  int j;
-  for(j=0; j < left/PGSIZE; j++){
-    printf("j=%d\n", j);
-    uintptr_t paddr = (uintptr_t)new_page(1);
-    map(&(pcb->as), (void *)(start+j*PGSIZE), (void *)paddr, 0);
-    memset((void *)paddr, 0, PGSIZE);
-  }assert(0);
-  //清零还剩一点
-  int left_memsz = left - j * PGSIZE;
-  uintptr_t paddr1 = (uintptr_t)new_page(1);
-  map(&(pcb->as), (void *)(start+j*PGSIZE), (void *)paddr1, 0);
-  memset((void *)paddr1, 0, left_memsz);
+  if(left > 0){
+    uintptr_t start = vaddr+(i+1)*PGSIZE;
+    int j;
+    for(j=0; j < left/PGSIZE; j++){
+      printf("j=%d\n", j);
+      uintptr_t paddr = (uintptr_t)new_page(1);
+      map(&(pcb->as), (void *)(start+j*PGSIZE), (void *)paddr, 0);
+      memset((void *)paddr, 0, PGSIZE);
+    }
+    //清零还剩一点
+    int left_memsz = left - j * PGSIZE;
+    uintptr_t paddr1 = (uintptr_t)new_page(1);
+    map(&(pcb->as), (void *)(start+j*PGSIZE), (void *)paddr1, 0);
+    memset((void *)paddr1, 0, left_memsz);
+  }
 }
 
 // static uintptr_t loader(PCB *pcb, const char *filename) {
