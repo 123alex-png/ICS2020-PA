b988cbee0e884484bcc4909f4f0266c10d94e0be
tracer-ics2020 <tracer@njuics.org>
2021-01-20 18:58:59 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:58:59 up  1:49,  1 user,  load average: 0.57, 0.50, 0.36

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -84,11 +84,11 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   // printf("pgdir = %p, pgtab = %p\n", pgdir, pgtab);
   
   if((pgtab[pgtab_index] & PTE_P) == 1){//是否存在这种情况？？？
-    printf("pgtab[%d] = %p\n， va: %p\n", pgtab_index, pgtab[pgtab_index], va);
+    // printf("pgtab[%d] = %p\n， va: %p\n", pgtab_index, pgtab[pgtab_index], va);
     panic("PTE_P of pgtab has been 1\n");
   }
   pgtab[pgtab_index] = ((uintptr_t)pa & ~0xfff) | PTE_P;
-  printf("pgtab[%d] = %p\n， va: %p\n", pgtab_index, pgtab[pgtab_index], va);
+  // printf("pgtab[%d] = %p\n， va: %p\n", pgtab_index, pgtab[pgtab_index], va);
 }
 
 
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,7 +25,7 @@ Elf_Ehdr ehdr;
 Elf_Phdr phdr;
 #define stdprot (0XFFFFFFFF)
 void page_load(int fd, PCB *pcb, uintptr_t vaddr, uint32_t filesz, uint32_t memsz){
-  // printf("vaddr = %p\n", vaddr);
+  printf("vaddr = %p\n", vaddr);
   uintptr_t align_vaddr = vaddr;
   if(vaddr%PGSIZE!=0){
     align_vaddr = ROUNDDOWN(vaddr, PGSIZE);
