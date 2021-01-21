801f67ddcadfaea92a623c6365a39d13e1e0f30d
tracer-ics2020 <tracer@njuics.org>
2021-01-21 16:32:33 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:32:33 up  8:56,  1 user,  load average: 0.33, 0.28, 0.21

--- navy-apps/apps/nterm/src/extern-sh.cpp
+++ navy-apps/apps/nterm/src/extern-sh.cpp
@@ -65,7 +65,6 @@ void extern_app_run(const char *app_path) {
       if (ch == '\n') break;
     }
     *p = '\0';
-    printf("%s\n", p);
     if (buf[0] == 'k') {
       const char *res = term->keypress(handle_key(buf + 1));
       if (res) {
