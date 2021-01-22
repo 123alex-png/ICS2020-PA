e0a23453e47376fbf4223d20f7d04929fddbcc5d
tracer-ics2020 <tracer@njuics.org>
2021-01-23 00:06:52 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  00:06:52 up 23:58,  1 user,  load average: 0.70, 0.33, 0.22

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -36,7 +36,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
 }
 
 void query_intr(DecodeExecState *s) {
-  if (cpu.eflags.IF && cpu.intr && false) {
+  if (cpu.eflags.IF && cpu.intr) {
     cpu.intr = false;
     raise_intr(s, IRQ_TIMER, cpu.pc);
     update_pc(s);
