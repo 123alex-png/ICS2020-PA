d7c09dce1e7529078e127514cb507fb6978926b4
tracer-ics2020 <tracer@njuics.org>
2021-01-22 23:44:50 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:44:50 up 23:36,  1 user,  load average: 0.32, 0.23, 0.09

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -14,13 +14,14 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   rtlreg_t tss_addr = (base_15_0) | (base_23_16 << 16) | (base_31_24 << 24);
   ksp = vaddr_read(tss_addr+4, 4);//tss.esp0
   // printf("nemu: ksp: 0x%x\n", ksp);
-  // rtl_mv(s, (rtlreg_t *)&ksp, &(cpu.edi));
+  rtl_mv(s, (rtlreg_t *)&ksp, &(cpu.edi));
   
-  // if(ksp != 0){
-  //   rtl_mv(s, (rtlreg_t *)&ksp, &(cpu.esp));
-  //   rtl_push(s, (rtlreg_t *)&(cpu.ss));
-  //   rtl_push(s, (rtlreg_t *)&(cpu.esp));
-  // }
+  if(ksp != 0){
+    rtl_mv(s, (rtlreg_t *)&(cpu.esp), s0);
+    rtl_mv(s, (rtlreg_t *)&ksp, &(cpu.esp));
+    rtl_push(s, (rtlreg_t *)&(cpu.ss));
+    rtl_push(s, s0);
+  }
 
   rtlreg_t addr=cpu.idtr.base+8*NO;
   rtlreg_t low=vaddr_read(addr,4);
