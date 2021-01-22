6bbbaadd457a4e657cb52097f4bbc13a4817e4b3
tracer-ics2020 <tracer@njuics.org>
2021-01-22 19:59:58 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:59:58 up 19:51,  1 user,  load average: 0.89, 0.77, 0.66

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -22,7 +22,7 @@ void __am_get_cur_as(Context *c);
 void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
-  // asm volatile("mov %%esp, %0":"=r"(c->esp3));//c->sp = $sp;
+  asm volatile("mov %%esp, %0":"=r"(c->esp3));//c->sp = $sp;
   //////get ksp///////
   
   __am_get_cur_as(c);
