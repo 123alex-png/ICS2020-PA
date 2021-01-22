c9066ca0dd0c6e2259e1f8df4ae109b25b4f725d
tracer-ics2020 <tracer@njuics.org>
2021-01-22 19:09:15 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:09:15 up 19:00,  1 user,  load average: 0.40, 0.27, 0.20

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -6,8 +6,8 @@
 #define SEG_KCODE      1
 #define SEG_KDATA      2
 #define NR_SEG 6
-static SegDesc gdt[NR_SEG] = {};
-static TSS32 tss = {};
+// static SegDesc gdt[NR_SEG] = {};
+// static TSS32 tss = {};
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
@@ -61,17 +61,17 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   // register event handler
   user_handler = handler;
 
-  // initialize GDT
-  gdt[1] = SEG32(STA_X | STA_R,   0,      0xffffffff, DPL_KERN);
-  gdt[2] = SEG32(STA_W,           0,      0xffffffff, DPL_KERN);
-  gdt[3] = SEG32(STA_X | STA_R,   0,      0xffffffff, DPL_USER);
-  gdt[4] = SEG32(STA_W,           0,      0xffffffff, DPL_USER);
-  gdt[5] = SEG16(STS_T32A,     &tss, sizeof(tss) - 1, DPL_KERN);
-  set_gdt(gdt, sizeof(gdt[0]) * NR_SEG);
+  // // initialize GDT
+  // gdt[1] = SEG32(STA_X | STA_R,   0,      0xffffffff, DPL_KERN);
+  // gdt[2] = SEG32(STA_W,           0,      0xffffffff, DPL_KERN);
+  // gdt[3] = SEG32(STA_X | STA_R,   0,      0xffffffff, DPL_USER);
+  // gdt[4] = SEG32(STA_W,           0,      0xffffffff, DPL_USER);
+  // gdt[5] = SEG16(STS_T32A,     &tss, sizeof(tss) - 1, DPL_KERN);
+  // set_gdt(gdt, sizeof(gdt[0]) * NR_SEG);
 
-  // initialize TSS
-  tss.ss0 = KSEL(2);
-  set_tr(KSEL(5));
+  // // initialize TSS
+  // tss.ss0 = KSEL(2);
+  // set_tr(KSEL(5));
   return true;
 }
 
