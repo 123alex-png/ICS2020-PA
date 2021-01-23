a0aef46d2712a0f9b309e8ac145c56e9e471671d
tracer-ics2020 <tracer@njuics.org>
2021-01-23 18:40:03 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:40:04 up 1 day,  5:57,  1 user,  load average: 0.53, 0.52, 0.46

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -2,7 +2,7 @@
 #include "local-include/rtl.h"
 
 #define IRQ_TIMER 32
-rtlreg_t ksp = 1;
+rtlreg_t ksp = 0;
 rtlreg_t tss_addr;
 void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
@@ -12,16 +12,16 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   
   if((cpu.cs & 0x3) == 0x3){
     // assert(0);
-    rtlreg_t gdt_addr=cpu.gdtr.base+8*(cpu.tr>>3);
-    rtlreg_t base_15_0 = vaddr_read(gdt_addr+2, 2) & 0xffff;
-    rtlreg_t base_23_16 = vaddr_read(gdt_addr+4, 1) & 0xff;
-    rtlreg_t base_31_24 = vaddr_read(gdt_addr+7, 1) & 0xff;
-    // printf("gdtr: %x, %x\n", cpu.gdtr.base, vaddr_read(cpu.gdtr.base, 4));
-    // printf("%x, %x, %x\n", base_15_0, base_23_16, base_31_24);
-    tss_addr = (base_15_0) | (base_23_16 << 16) | (base_31_24 << 24);
-    printf("tss_addr: %x\n,", tss_addr);
+    // rtlreg_t gdt_addr=cpu.gdtr.base+8*(cpu.tr>>3);
+    // rtlreg_t base_15_0 = vaddr_read(gdt_addr+2, 2) & 0xffff;
+    // rtlreg_t base_23_16 = vaddr_read(gdt_addr+4, 1) & 0xff;
+    // rtlreg_t base_31_24 = vaddr_read(gdt_addr+7, 1) & 0xff;
+    // // printf("gdtr: %x, %x\n", cpu.gdtr.base, vaddr_read(cpu.gdtr.base, 4));
+    // // printf("%x, %x, %x\n", base_15_0, base_23_16, base_31_24);
+    // tss_addr = (base_15_0) | (base_23_16 << 16) | (base_31_24 << 24);
+    // printf("tss_addr: %x\n,", tss_addr);
     // ksp = vaddr_read(tss_addr+4, 4);//tss.esp0
-    printf("ksp: %x, cs: %x\n", ksp, cpu.cs);
+    // printf("ksp: %x, cs: %x\n", ksp, cpu.cs);
 
     rtl_mv(s, (rtlreg_t *)&(cpu.esp), s0);
     rtl_mv(s, (rtlreg_t *)&(cpu.ss), s1);
@@ -32,7 +32,8 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
     printf("push: esp = %x, ss = %x\n", *s0, *s1);
     // while(1);
   }
-  vaddr_write(tss_addr+4, 0, 4);
+  ksp = 0;
+  // vaddr_write(tss_addr+4, 0, 4);
   rtlreg_t addr=cpu.idtr.base+8*NO;
   rtlreg_t low=vaddr_read(addr,4);
   rtlreg_t high=vaddr_read(addr+4,4);
