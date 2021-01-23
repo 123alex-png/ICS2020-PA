d1f3de086d81250f800967513553b02447cedaca
tracer-ics2020 <tracer@njuics.org>
2021-01-23 16:57:37 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:57:37 up 1 day,  5:34,  1 user,  load average: 0.87, 0.49, 0.24

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -13,9 +13,9 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   if((cpu.cs & 0x3) == 0x3){
     // assert(0);
     rtlreg_t gdt_addr=cpu.gdtr.base+8*cpu.tr;
-    rtlreg_t base_15_0 = vaddr_read(gdt_addr+2, 2) & 0xff;
-    rtlreg_t base_23_16 = vaddr_read(gdt_addr+4, 1) & 0xf;
-    rtlreg_t base_31_24 = vaddr_read(gdt_addr+7, 1) & 0xf;
+    rtlreg_t base_15_0 = vaddr_read(gdt_addr+2, 2) & 0xffff;
+    rtlreg_t base_23_16 = vaddr_read(gdt_addr+4, 1) & 0xff;
+    rtlreg_t base_31_24 = vaddr_read(gdt_addr+7, 1) & 0xff;
     printf("%x, %x, %x\n", base_15_0, base_23_16, base_31_24);
     tss_addr = (base_15_0) | (base_23_16 << 16) | (base_31_24 << 24);
     ksp = vaddr_read(tss_addr+4, 4);//tss.esp0
