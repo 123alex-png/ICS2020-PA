991cb70d831e8a490817d2b7a3de03316255c5ee
tracer-ics2020 <tracer@njuics.org>
2021-01-23 13:37:45 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:37:46 up 1 day,  2:14,  1 user,  load average: 0.24, 0.23, 0.35

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -55,9 +55,9 @@ static inline def_EHelper(iret) {
   rtl_pop(s,&(cpu.eflag_val));
   // printf("cs: %x\n", cpu.cs);
   if((cpu.cs & 0x3) == 0x3){//用户态
-    rtl_pop(s,s0);
+    // rtl_pop(s,s0);
     rtl_pop(s,(rtlreg_t *)&(cpu.ss));
-    rtl_mv(s, s0, &(cpu.esp));
+    // rtl_mv(s, s0, &(cpu.esp));
     // vaddr_write(tss_addr+4, *s0, 4);
     printf("pop: esp = %x, ss = %x\n", cpu.esp, cpu.ss);
   }
