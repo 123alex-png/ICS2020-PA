0582373f7b75864955852274052fc11063b2a262
tracer-ics2020 <tracer@njuics.org>
2021-01-24 11:20:01 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:20:01 up 1 day, 16:39,  1 user,  load average: 1.47, 0.92, 0.57

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,7 +33,7 @@ void init_proc() {
   
   char *arg[]={/*"/bin/exec-test", "12", "/bin/menu",*/NULL};
   context_uload(&pcb[0], "/bin/nterm", arg, NULL);
-  context_uload(&pcb[1], "/bin/pal", arg, NULL);
+  // context_uload(&pcb[1], "/bin/pal", arg, NULL);
   // context_uload(&pcb[2], "/bin/hello", arg, NULL);
   switch_boot_pcb();
 
@@ -46,7 +46,7 @@ void init_proc() {
 Context* schedule(Context *prev) {
 
   current->cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = &pcb[0];//(current == &pcb[0] ? &pcb[1] : &pcb[0]);
   // current = current == &pcb[0] ? (current == &pcb[1] ? &pcb[2] : &pcb[1]) : &pcb[0];
   return current->cp;
 }
