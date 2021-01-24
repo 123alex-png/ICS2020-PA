096621af48e0d31c8640f22f458863b28fe5faab
tracer-ics2020 <tracer@njuics.org>
2021-01-24 22:01:18 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:01:18 up 2 days,  2:09,  1 user,  load average: 0.40, 0.50, 0.52

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -12,14 +12,14 @@ void call_main(uintptr_t *args) {
   }
   else{
     char **argv = (char **)(args + 1);
-    for(int i = 0; i < *args; i++){
-      for(int j = 0; argv[i][j] != '\0'; j++){
-        if(argv[i][j]==1){
-          argv[i][j]='\0';
-          break;
-        }
-      }
-    }
+    // for(int i = 0; i < *args; i++){
+    //   for(int j = 0; argv[i][j] != '\0'; j++){
+    //     if(argv[i][j]==1){
+    //       argv[i][j]='\0';
+    //       break;
+    //     }
+    //   }
+    // }
     printf("crt0: argv = %s\n", argv[0]);
     // while(1);
     // printf("argc = %d\n", *(int *)args);
