a66c29199c78d460ef5d7d88d2ff042f8525922b
tracer-ics2020 <tracer@njuics.org>
2021-01-24 09:19:10 +0800

    >  compile 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:19:10 up 1 day, 14:38,  1 user,  load average: 0.35, 0.40, 0.48

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -56,13 +56,13 @@ static inline def_EHelper(iret) {
   rtl_pop(s,&(cpu.eflag_val));
   // printf("cs: %x\n", cpu.cs);
   if((cpu.cs & 0x3) == 0x3){//用户态
-    printf("jump to %x\n", *s0);
+    // printf("jump to %x\n", *s0);
     rtl_pop(s,s1);
-    printf("s1: %x\n", *s1);
+    // printf("s1: %x\n", *s1);
     // vaddr_write(tss_addr+4, *s1, 4);
     rtl_pop(s,(rtlreg_t *)&cpu.ss);
     rtl_mv(s, &(cpu.esp), s1); 
-    printf("pop: esp = %x, ss = %x\n", cpu.esp, cpu.ss);
+    // printf("pop: esp = %x, ss = %x\n", cpu.esp, cpu.ss);
   }
   
   rtl_j(s,*s0);
