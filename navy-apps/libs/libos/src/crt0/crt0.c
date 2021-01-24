7fb516f20104ad9b25cc3403728f882e6d932cba
tracer-ics2020 <tracer@njuics.org>
2021-01-24 12:13:02 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:13:03 up 1 day, 17:32,  1 user,  load average: 0.60, 0.47, 0.52

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
+  assert(0);
   char *empty[] =  {NULL };
   environ = empty;
   if(args == NULL) {
