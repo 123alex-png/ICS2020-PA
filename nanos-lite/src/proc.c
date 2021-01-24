8ec215741895f9a1e5e606a9b36a65312b694625
tracer-ics2020 <tracer@njuics.org>
2021-01-24 22:12:50 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:12:50 up 2 days,  2:21,  1 user,  load average: 0.66, 0.65, 0.60

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,11 +37,11 @@ void init_proc() {
 
   // char *arg[]={"/bin/exec-test", "12",NULL};
   char *arg[]={NULL};
-  context_uload(&pcb[0], "/bin/nterm", arg, NULL);
-  // context_uload(&pcb[0], "/bin/hello", arg, NULL);
-  // context_uload(&pcb[1], "/bin/pal", arg, NULL);
-  // context_uload(&pcb[2], "/bin/bird", arg, NULL);
-  // context_uload(&pcb[3], "/bin/nslider", arg, NULL);
+  // context_uload(&pcb[0], "/bin/nterm", arg, NULL);
+  context_uload(&pcb[0], "/bin/hello", arg, NULL);
+  context_uload(&pcb[1], "/bin/pal", arg, NULL);
+  context_uload(&pcb[2], "/bin/bird", arg, NULL);
+  context_uload(&pcb[3], "/bin/nslider", arg, NULL);
 
   switch_boot_pcb();
   Log("Initializing processes...");
@@ -50,7 +50,7 @@ void init_proc() {
 Context* schedule(Context *prev) {
 
   current->cp = prev;
-  // current = (current == &pcb[0] ? fg_pcb : &pcb[0]);
-  current = &pcb[0];
+  current = (current == &pcb[0] ? fg_pcb : &pcb[0]);
+  // current = &pcb[0];
   return current->cp;
 }
