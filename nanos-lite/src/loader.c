79ed6de36b77f577460d9c27c1d65397820adf97
tracer-ics2020 <tracer@njuics.org>
2021-01-24 15:11:51 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:11:51 up 1 day, 20:30,  1 user,  load average: 0.61, 0.60, 0.47

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -94,7 +94,7 @@ void context_kload(PCB *pcb, void *entry, void *arg){
 int cnt = 0;
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
   printf("filename:%s\n", filename);
-  assert(++cnt == 1);
+  assert(0);
   protect(&(pcb->as));
   char *tmp[80];
   for(int i = 0; i < 80; i++){
