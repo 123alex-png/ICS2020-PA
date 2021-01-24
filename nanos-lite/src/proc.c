3356447d95d95cc6495e2942435f5a612dde969a
tracer-ics2020 <tracer@njuics.org>
2021-01-24 11:38:20 +0800

    >  run 191870147    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 2e0b7f30c2ad44c98b8f7475c1f60dc7            Boot ID: 9f0f0ec09d444dbcafa2b30feb4511b4     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:38:20 up 1 day, 16:57,  1 user,  load average: 0.45, 0.49, 0.52

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,12 +32,11 @@ void init_proc() {
   // context_kload(&pcb[1], (void *)hello_fun, (void *)52314);
   
   char *arg[]={/*"/bin/exec-test", "12", "/bin/menu",*/NULL};
-  context_uload(&pcb[1], "/bin/pal", arg, NULL);
-  // context_uload(&pcb[1], "/bin/pal", arg, NULL);
+  context_uload(&pcb[0], "/bin/pal", arg, NULL);
+  context_uload(&pcb[1], "/bin/hello", arg, NULL);
   
-  context_uload(&pcb[2], "/bin/hello", arg, NULL);
+  // context_uload(&pcb[2], "/bin/hello", arg, NULL);
   switch_boot_pcb();
-  assert(0);
   Log("Initializing processes...");
 }
 
